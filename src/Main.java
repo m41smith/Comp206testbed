307d4beb02096c6d5010890ba6f891e1339984af
Marcus <m41smith@student.bridgew.edu>
2022-05-26 14:53:09 -0400

    initial

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        double A = 0;
+        double B = 0;
+        while(A < 255){
+            A = Math.pow(2, B);
+            B++;
+            System.out.println(A);
+        }
     }
 }
