c1ba9ff94b54f8a44c057e9533f8070d60433fb2
m41smith <m41smith@student.bridgew.edu>
2022-05-26 17:45:24 -0400

    next

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,24 @@
 public class Main {
     public static void main(String[] args) {
         double A = 0;
-        double B = 0;
-        System.out.println("Pow");
-        while(A < 255){
-            A = Math.pow(2, B);
-            B++;
+        double B = 1;
+        double C = 0;
+//        System.out.println("fib");
+//        while (A < 255){
+//            C = A + B;
+//            B = A;
+//            A = C;
+//            System.out.println(A);
+//        }
+//        A = 0;
+//        B = 1;
+//        C = 0;
+        System.out.println("fib by add");
+        while (A < 255){
+            A = A + B;
+            B = B + A;
             System.out.println(A);
-        }
-        System.out.println("Pow with add");
-        double C = 1;
-        while(C < 255){
-            C = C + C;
-            System.out.println(C);
-
+            System.out.println(B);
         }
     }
 }
