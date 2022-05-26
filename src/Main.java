372a9f92b35e89a4bce52497ccef4aee0187d325
Marcus <m41smith@student.bridgew.edu>
2022-05-26 15:30:40 -0400

    5/26

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,18 @@ public class Main {
     public static void main(String[] args) {
         double A = 0;
         double B = 0;
+        System.out.println("Pow");
         while(A < 255){
             A = Math.pow(2, B);
             B++;
             System.out.println(A);
+        }
+        System.out.println("Pow with add");
+        double C = 1;
+        while(C < 255){
+            C = C + C;
+            System.out.println(C);
+
         }
     }
 }
